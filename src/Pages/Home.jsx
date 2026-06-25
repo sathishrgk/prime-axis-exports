2346b3032dd8794b9329d7757803accf33e8a2a5
sathishrgk@gmail.com <sathishrgk@gmail.com>
2026-06-25 15:24:59 +0530

    Updated homepage with Prime Axis logo and export content

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/prime-axis-exports.png";
+import logo from "../assets/Prime-axis-exports.png";
 
 
 export default function Home() {
