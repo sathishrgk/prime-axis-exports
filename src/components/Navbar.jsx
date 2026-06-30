0228ccb6d056a78eccfae358315c394f4c5c6b74
sathishrgk@gmail.com <sathishrgk@gmail.com>
2026-06-30 10:39:39 +0530

    Updated contact and about pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/Prime-axis-exports.png";
+import logo from "../assets/prime-axis-exports.png";
 
 
 export default function Navbar(){
 
+
+const [open,setOpen] = useState(false);
+
+
+
 return(
 
-<nav className="bg-green-900 px-8 py-4 flex items-center justify-between">
+<nav className="bg-green-900 px-6 md:px-10 py-4">
 
 
-{/* Logo + Company Name */}
+<div className="flex items-center justify-between">
 
-<div className="flex items-center gap-3">
+
+
+
+
+{/* Logo Section */}
+
+
+<Link to="/" className="flex items-center gap-3">
 
 
 <img
@@ -20,7 +33,7 @@ src={logo}
 
 alt="Prime Axis Exports"
 
-className="w-20 h-20 object-contain"
+className="w-16 h-16 md:w-20 md:h-20 object-contain"
 
 />
 
@@ -28,14 +41,14 @@ className="w-20 h-20 object-contain"
 <div>
 
 
-<h1 className="text-2xl font-bold text-white">
+<h1 className="text-xl md:text-3xl font-bold text-white">
 
-PRIME AXIS EXPORTS
+Prime Axis Exports
 
 </h1>
 
 
-<p className="text-sm text-white tracking-wide">
+<p className="text-xs md:text-sm text-white tracking-wide">
 
 PREMIUM EXPORTS • GLOBAL REACH
 
@@ -45,47 +58,160 @@ PREMIUM EXPORTS • GLOBAL REACH
 </div>
 
 
-</div>
+</Link>
 
 
 
 
 
-{/* Menu */}
-
-<div className="flex gap-6 text-white">
 
 
-<Link to="/">
+
+{/* Mobile Menu Button */}
+
+
+<button
+
+onClick={()=>setOpen(!open)}
+
+className="md:hidden text-white text-3xl"
+
+>
+
+
+☰
+
+
+</button>
+
+
+
+
+
+
+
+{/* Desktop Menu */}
+
+
+<div className="hidden md:flex gap-8 text-white font-medium">
+
+
+<Link to="/" className="hover:text-yellow-400">
+
 Home
+
 </Link>
 
 
-<Link to="/about">
+<Link to="/about" className="hover:text-yellow-400">
+
 About
+
 </Link>
 
 
-<Link to="/products">
+<Link to="/products" className="hover:text-yellow-400">
+
 Products
+
 </Link>
 
 
-<Link to="/export">
+<Link to="/export" className="hover:text-yellow-400">
+
 Export
+
 </Link>
 
 
-<Link to="/contact">
+<Link to="/contact" className="hover:text-yellow-400">
+
 Contact
+
 </Link>
 
 
 </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+{/* Mobile Menu */}
+
+
+{open && (
+
+<div className="md:hidden mt-5 flex flex-col gap-4 text-white font-medium">
+
+
+<Link 
+onClick={()=>setOpen(false)}
+to="/"
+>
+
+Home
+
+</Link>
+
+
+<Link
+onClick={()=>setOpen(false)}
+to="/about"
+>
+
+About
+
+</Link>
+
+
+<Link
+onClick={()=>setOpen(false)}
+to="/products"
+>
+
+Products
+
+</Link>
+
+
+<Link
+onClick={()=>setOpen(false)}
+to="/export"
+>
+
+Export
+
+</Link>
+
+
+<Link
+onClick={()=>setOpen(false)}
+to="/contact"
+>
+
+Contact
+
+</Link>
+
+
+
+</div>
+
+)}
 
 
 
 </nav>
+
 
 )
 
